c07ccf29bce9932864e7c9b1a7b625b2e689ea1b
vitavoloshchuk <vitavoloshchuk@gmail.com>
2023-07-18 15:30:20 -0400

    finished US244 login

--- src/test/java/com/CRM_Archer_B29/step_definitions/Login_StepDef.java
+++ src/test/java/com/CRM_Archer_B29/step_definitions/Login_StepDef.java
@@ -1,11 +1,14 @@
 package com.CRM_Archer_B29.step_definitions;
 
 import com.CRM_Archer_B29.pages.LoginPage;
+import com.CRM_Archer_B29.utilities.BrowserUtils;
+import com.CRM_Archer_B29.utilities.ConfigReader;
 import com.CRM_Archer_B29.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class Login_StepDef {
@@ -14,12 +17,12 @@ public class Login_StepDef {
     //    sc1
     @Given("User is on the login page")
     public void user_is_on_the_login_page() {
-        Driver.getDriver().get("environment");
+        Driver.getDriver().get(ConfigReader.getProperty("environment"));
     }
 
     @When("User enters a valid {string} and {string}")
     public void user_enters_a_valid_and(String user, String pass) {
-
+        loginPage.loginWPass(user, pass);
     }
 
     @When("User clicks login button")
@@ -29,57 +32,69 @@ public class Login_StepDef {
 
     @Then("User should land on a home page")
     public void user_should_land_on_a_home_page() {
-        String actualURL = Driver.getDriver().getCurrentUrl();
         String expectedURL = "stream";
-        Assert.assertTrue(expectedURL.contains(actualURL));
+        String actualURL = Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(actualURL.contains(expectedURL));
     }
 
 
     //sc2
 
     @When("User enters invalid {string} or invalid {string}")
-    public void userEntersInvalidOrInvalid(String arg0, String arg1) {
-
+    public void userEntersInvalidOrInvalid(String invalidUser, String invalidPass) {
+        loginPage.loginWPass(invalidUser, invalidPass);
     }
 
+    @Then("User should see message Incorrect login or password")
+    public void userShouldSeeMessageIncorrectLoginOrPassword() {
+        String expectedMsg = "Incorrect login or password";
+        String actualMsg = loginPage.errorMessage.getText();
 
-    @Then("User should see message {string}")
-    public void user_should_see_message(String string) {
-
-
+        Assert.assertEquals(expectedMsg, actualMsg);
     }
 
 
     //sc3
 
     @When("User enters only a  {string} or {string}")
-    public void user_enters_only_a_or(String string, String string2) {
-
+    public void user_enters_only_a_or(String username, String password) {
+        loginPage.loginWPass(username, password);
     }
 
+    @Then("User should see message Please fill out this field")
+    public void userShouldSeeMessagePleaseFillOutThisField() {
+        String actualMsg = loginPage.errorMessage.getText();
+        String expectedMsg = "Please fill out this field";
 
+        Assert.assertEquals(expectedMsg, actualMsg);
+    }
     //sc4
 
-    @When("User locates {string} link")
-    public void user_locates_link(String string) {
-
+    @When("User clicks Remember Me link")
+    public void userClicksRememberMeLink() {
+        loginPage.rememberMe.click();
     }
 
-    @Then("User clicks {string} link")
-    public void user_clicks_link(String string) {
-
+    @Then("Remember Me link is selected")
+    public void rememberMeLinkIsSelected() {
+        Assert.assertTrue(loginPage.rememberMe.isEnabled());
     }
 
 
     //sc5
 
     @When("User enters any {string} and {string}")
-    public void user_enters_any_and(String string, String string2) {
-
+    public void user_enters_any_and(String username, String password) {
+        loginPage.loginWPass(username, password);
     }
 
     @Then("password is in bullet signs")
     public void password_is_in_bullet_signs() {
+        String expectedAttributeValue = "password";
+        String actAttributeValue = loginPage.hiddenPass.getAttribute("type");
 
+        Assert.assertEquals(expectedAttributeValue, actAttributeValue);
     }
+
+
 }
