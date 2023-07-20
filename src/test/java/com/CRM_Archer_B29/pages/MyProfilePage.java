9c1530185f1177535a868a569d4eb3754f2788c0
Hido_Cydeo <hidoyatnuridinova@gmail>
2023-07-19 20:09:10 -0400

    My Profile page

--- src/test/java/com/CRM_Archer_B29/pages/MyProfilePage.java
+++ src/test/java/com/CRM_Archer_B29/pages/MyProfilePage.java
@@ -1,10 +1,28 @@
 package com.CRM_Archer_B29.pages;
 
 import com.CRM_Archer_B29.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class MyProfilePage {
     public MyProfilePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+
+    @FindBy(id = "user-block")
+    public WebElement profile;
+
+    @FindBy(xpath = "//span[.='My Profile']")
+    public WebElement MyProfile;
+
+    @FindBy(xpath = "//div[@id='profile-menu-filter']/a")
+    public List<WebElement> menuProfile;
+
+    @FindBy(xpath = "//*[@id=\"workarea-content\"]/div/div/div[3]/table/tbody/tr[2]/td[2]/a")
+    public WebElement email;
+
 }
--- src/test/java/com/CRM_Archer_B29/step_definitions/ProfilePage_StepDefinitions.java
+++ src/test/java/com/CRM_Archer_B29/step_definitions/ProfilePage_StepDefinitions.java
@@ -1,25 +1,50 @@
 package com.CRM_Archer_B29.step_definitions;
 
+import com.CRM_Archer_B29.pages.LoginPage;
+import com.CRM_Archer_B29.pages.MyProfilePage;
+import com.CRM_Archer_B29.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ProfilePage_StepDefinitions {
 
-    @Given("user click on the username button")
-    public void user_click_on_the_username_button() {
+    LoginPage loginPage = new LoginPage();
+    MyProfilePage myProfilePage = new MyProfilePage();
 
+
+    @Given("user clicks on his or her profile")
+    public void userClicksOnHisHerProfile() {
+        loginPage.allInLogin("hr","2");
+        myProfilePage.profile.click();
     }
+
+
     @Then("user click on the My profile")
     public void user_click_on_the_my_profile() {
-
+       myProfilePage.MyProfile.click();
     }
-    @Then("user see the following options")
-    public void user_see_the_following_options(io.cucumber.datatable.DataTable dataTable) {
 
+    @Then("user should to see following options")
+    public void user_should_to_see_following_options(List<String> expectedModulus) {
+        List<String> a = new ArrayList<>();
+        for (WebElement webElement : myProfilePage.menuProfile) {
+            a.add(webElement.getText());
+        }
+        Assert.assertEquals(a,expectedModulus);
     }
 
     @Given("user see the email under the General tab")
     public void user_see_the_email_under_the_general_tab() {
+
+        String actualEmail = myProfilePage.email.getText();
+        String expectedEmail = "hr2@cybertekschool.com";
+        Assert.assertEquals(actualEmail, expectedEmail);
 
     }
 }
