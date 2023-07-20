db944472bfdee7acd4f90d400144bf2be4b07ecb
anis_k <aanniissk@gmail.com>
2023-07-20 14:11:54 -0400

    added

--- src/test/java/com/CRM_Archer_B29/pages/HomePage.java
+++ src/test/java/com/CRM_Archer_B29/pages/HomePage.java
@@ -11,7 +11,8 @@ public class HomePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-
+    @FindBy(xpath = "//span[normalize-space(text())='Activity Stream']")
+    public WebElement activityStreamBtn;
     @FindBy(id = "user-block")
     public WebElement UsertabOptions;
     @FindBy(xpath = "(//a[@class='menu-popup-item menu-popup-no-icon '])[3]")
