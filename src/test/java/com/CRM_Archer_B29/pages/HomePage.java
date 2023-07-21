a13e8a570a54672181a1bc8b9a76bee34dca62d3
anis_k <aanniissk@gmail.com>
2023-07-21 09:48:45 -0400

    added

--- src/test/java/com/CRM_Archer_B29/pages/HomePage.java
+++ src/test/java/com/CRM_Archer_B29/pages/HomePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.logging.XMLFormatter;
+
 public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(), this);
@@ -17,4 +19,10 @@ public class HomePage {
     public WebElement UsertabOptions;
     @FindBy(xpath = "(//a[@class='menu-popup-item menu-popup-no-icon '])[3]")
     public WebElement LogoutBtn;
+    @FindBy(id = "feed-add-post-form-tab-message")
+    public WebElement MessageTABE;
+@FindBy(id = "blog-submit-button-save")
+    public WebElement SendMessageBTN;
+@FindBy(xpath = "//span[@class='feed-add-info-text']")
+    public WebElement ATleastOnePersonERRORM;
 }
