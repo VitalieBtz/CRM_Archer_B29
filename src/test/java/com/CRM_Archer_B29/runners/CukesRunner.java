38077d86ea6dc3fdf0dd6161f2201aa9ccaf0b2e
VitalieBtz <botezatu.v@gmail.com>
2023-07-21 08:22:43 -0400

    cukes runner tags

--- src/test/java/com/CRM_Archer_B29/runners/CukesRunner.java
+++ src/test/java/com/CRM_Archer_B29/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        tags = "@B29G6-272",
+        tags = "",
         dryRun = false,
         features = "src/test/resources/features",
         glue = "com/CRM_Archer_B29/step_definitions",
