67d1ae428f8b553e4dbac15972ce004ae79c515c
sara54c <sara5453c@gmail.com>
2023-07-25 00:10:48 -0400

    created Appreciation Message Step Definition

---
+++ src/test/java/com/CRM_Archer_B29/step_definitions/AppreciationMessage_StepDef.java
@@ -0,0 +1,55 @@
+package com.CRM_Archer_B29.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AppreciationMessage_StepDef {
+    @Then("user clicks on MORE module")
+    public void userClicksOnMOREModule() {
+    }
+
+    @And("user clicks Appreciation")
+    public void userClicksAppreciation() {
+    }
+
+    @When("user enters {string} in Message Content which is a mandatory field.")
+    public void userEntersInMessageContentWhichIsAMandatoryField(String arg0) {
+    }
+
+    @When("user left the message field empty")
+    public void userLeftTheMessageFieldEmpty() {
+    }
+
+    @Then("user clicks send button.")
+    public void userClicksSendButton() {
+    }
+
+    @And("user verifies that mandatory field error message:The message title is not specified")
+    public void userVerifiesThatMandatoryFieldErrorMessageTheMessageTitleIsNotSpecified() {
+    }
+
+    @Then("user removes All employees recipient which is default")
+    public void userRemovesAllEmployeesRecipientWhichIsDefault() {
+    }
+
+    @Then("user verifies that mandatory field error message:Please specify at least one person")
+    public void userVerifiesThatMandatoryFieldErrorMessagePleaseSpecifyAtLeastOnePerson() {
+    }
+
+    @And("verifies user can see {string} which is the appreciation message under Activity Stream.")
+    public void verifiesUserCanSeeWhichIsTheAppreciationMessageUnderActivityStream(String arg0) {
+    }
+
+    @And("user verifies that the delivery should be to All employees by default.")
+    public void userVerifiesThatTheDeliveryShouldBeToAllEmployeesByDefault() {
+    }
+
+    @Then("user clicks cancel button")
+    public void userClicksCancelButton() {
+    }
+
+    @And("user verifies that user should be able to cancel sending appreciation at any time before sending.")
+    public void userVerifiesThatUserShouldBeAbleToCancelSendingAppreciationAtAnyTimeBeforeSending() {
+    }
+}
