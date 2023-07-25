e7f9568d46b1121544af09005f3a1a84c69f60a5
sara54c <sara5453c@gmail.com>
2023-07-24 23:56:51 -0400

    created Appreciation Message feature

--- src/test/java/com/CRM_Archer_B29/step_definitions/AppreciationMessage_StepDefinition.java
+++ src/test/java/com/CRM_Archer_B29/step_definitions/AppreciationMessage_StepDefinition.java
@@ -13,112 +13,13 @@ import org.openqa.selenium.WebElement;
 
 public class AppreciationMessage_StepDefinition {
 
-
-    ActivityStreamPage activityStreamPage = new ActivityStreamPage();
-
-    @Then("user clicks on MORE module")
-    public void userClicksOnMOREModule() {
-        BrowserUtils.sleep(2);
-        activityStreamPage.moreModule.click();
-    }
-
-
-    @And("user clicks Appreciation")
-    public void userClicksAppreciation() {
-        BrowserUtils.waitForVisibility(activityStreamPage.appreciationPopUp,2);
-        activityStreamPage.appreciationPopUp.click();
-    }
-
-
-    @When("user enters {string} in Message Content which is a mandatory field.")
-    public void userEntersInMessageContentWhichIsAMandatoryField(String message) {
-        BrowserUtils.sleep(2);
-        WebElement iframe = Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']"));
-        //Driver.getDriver().switchTo().frame(0);
-        Driver.getDriver().switchTo().frame(iframe);
-        BrowserUtils.sleep(2);
-
-
-
-        BrowserUtils.sleep(2);
-
-
-        Driver.getDriver().switchTo().defaultContent();
-
-
-    }
-
-    @Then("user choose recipient in To field which is a mandatory field")
-    public void userChooseRecipientInToFieldWhichIsAMandatoryField() {
-
-
-    }
-
-    @Then("user clicks send button.")
-    public void userClicksSendButton() {
-        //BrowserUtils.waitForVisibility(activityStreamPage.appreciationPopUp,2);
-
-        BrowserUtils.sleep(2);
-        activityStreamPage.sendBtn.click();
-
-
-    }
-    @And("verifies user can see {string} which is the appreciation message under Activity Stream.")
-    public void verifiesUserCanSeeWhichIsTheAppreciationMessageUnderActivityStream(String message) {
-        BrowserUtils.sleep(1);
-        WebElement appreciationMsg = Driver.getDriver().findElement(By.xpath("//div[contains(@id, 'blog_post_body')]//a[.=\'"+message+"\']"));
-        Assert.assertTrue(appreciationMsg.isDisplayed());
-
-    }
-
-
-    @Then("user does not choose any recipient in To field which is a mandatory field")
-    public void userDoesNotChooseAnyRecipientInToFieldWhichIsAMandatoryField() {
-        
-    }
-
-    @Then("user verifies that mandatory field error message:Please specify at least one person")
-    public void userVerifiesThatMandatoryFieldErrorMessagePleaseSpecifyAtLeastOnePerson() {
-    }
-
-    @When("user left the message field empty")
-    public void userLeftTheMessageFieldEmpty() {
-    }
-
-    @And("user verifies that mandatory field error message:The message title is not specified")
-    public void userVerifiesThatMandatoryFieldErrorMessageTheMessageTitleIsNotSpecified() {
-        String expectedText = "The message title is not specified";
-        WebElement errorMessageTitle=Driver.getDriver().findElement(By.cssSelector(".feed-add-error"));
-        String actualText = errorMessageTitle.getText();
-        Assert.assertEquals(actualText, expectedText,"Error Message title is not shown as expected");
-
-    }
-
-    @And("user verifies that the delivery should be to All employees by default.")
-    public void userVerifiesThatTheDeliveryShouldBeToAllEmployeesByDefault() {
-        WebElement allEmployees = Driver.getDriver().findElement(By.cssSelector(".feed-add-post-destination-text"));
-        Assert.assertTrue(allEmployees.isDisplayed());
-    }
-
-    @Then("user clicks cancel button")
-    public void userClicksCancelButton() {
-        Driver.getDriver().switchTo().parentFrame();
-        WebElement cancelBtn = Driver.getDriver().findElement(By.cssSelector("#blog-submit-button-cancel"));
-        cancelBtn.click();
-
-
-
-    }
-
-    @And("user verifies that user should be able to cancel sending appreciation at any time before sending.")
-    public void userVerifiesThatUserShouldBeAbleToCancelSendingAppreciationAtAnyTimeBeforeSending() {
-
-        WebElement msgContent = Driver.getDriver().findElement(By.xpath("//body[@contenteditable='true']"));
-        BrowserUtils.sleep(2);
-        Assert.assertFalse(msgContent.isDisplayed());
-    }
-
-
 }
+
+
+
+
+
+
+
 
 
--- target/test-classes/features/LoginPC.feature
+++
@@ -1,5 +0,0 @@
-Feature: As a user I should be able to login in
-
-  @login
-  Scenario:
-    Given user is on the login page
